f7f502a08978953aa13e373bea0f010fdcc0f816
mikewang928 <55117208+mikewang928@users.noreply.github.com>
2020-08-26 18:37:46 +0800

    add Calculator V2.0 in test

---
+++ src/Chapter_12/Calculator_2_0.java
@@ -0,0 +1,182 @@
+package Chapter_12;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Calculator_2_0 {
+    public static void main(String[] args){
+        CalculatorFrame_2_0 frame = new CalculatorFrame_2_0();
+        frame.setTitle("CalculatorV2.0");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
+
+class CalculatorFrame_2_0 extends JFrame{
+    Toolkit kit = Toolkit.getDefaultToolkit();
+    Dimension screenSize = kit.getScreenSize();
+    final int DEFAULT_WIDTH = (int)(screenSize.getWidth()*0.2);
+    final int DEFAULT_HEIGHT =(int)(screenSize.getHeight()*0.2);
+    public CalculatorFrame_2_0(){
+        setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+        CalculatorPanel_2_0 Cpanel = new CalculatorPanel_2_0();
+        add(Cpanel);
+    }
+}
+
+/**
+ * A panel with calculator buttons and a result display
+ */
+
+class CalculatorPanel_2_0 extends JPanel{
+    private JButton display;
+    private JPanel panel;
+    private double result;
+    private String lastCommand;
+    private boolean start;
+
+    public CalculatorPanel_2_0(){
+        setLayout(new BorderLayout());
+
+        result = 0;
+        lastCommand = "=";
+        start = true;
+
+        //add the display
+
+        display = new JButton("");
+        display.setEnabled(false);
+        add(display, BorderLayout.NORTH);
+
+        ActionListener insert = new InsertAction();
+//        ActionListener del = new DeletAction();
+//        ActionListerer clear = new ClearAction();
+        ActionListener command = new CommandAction();
+
+        // add the buttons in a 4*4 grid
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(5,4));
+
+        addButton("A/C",command);
+        addButton("%",insert);
+        addButton("+/-",insert);
+        addButton("<-",insert);
+
+        addButton("7",insert);
+        addButton("8",insert);
+        addButton("9",insert);
+        addButton("/",command);
+
+        addButton("4",insert);
+        addButton("5",insert);
+        addButton("6",insert);
+        addButton("*",command);
+
+        addButton("1",insert);
+        addButton("2",insert);
+        addButton("3",insert);
+        addButton("-",command);
+
+        addButton("0",insert);
+        addButton(".",insert);
+        addButton("=",command);
+        addButton("+",command);
+
+        add(panel, BorderLayout.CENTER);
+    }
+
+    /**
+     * Adds a button to the center
+     * @param label
+     * @param listener
+     */
+    private void addButton(String label, ActionListener listener){
+        JButton button = new JButton(label);
+        button.addActionListener(listener);
+        panel.add(button);
+    }
+
+    /**
+    private class DeletAction implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+
+        }
+    }
+    */
+
+
+    /**
+     * this class inserts the button action string to the end of the display text.
+     */
+    private class InsertAction implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            String input = event.getActionCommand();
+            if(start){
+                display.setText("");
+                start = false;
+            }
+            if(input.equals("<-")){
+                if(display.getText()==""){
+                    display.setText("");
+                }
+                else{
+                    display.setText(display.getText().substring(0,display.getText().length()-1));
+                }
+            }
+            else {
+                display.setText(display.getText() + input);
+            }
+        }
+    }
+
+    /**
+     * This action executes the command that the button action string denotes
+     */
+    private class CommandAction implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            String command = event.getActionCommand();
+
+            if(start){
+                if(command.equals("-")||command.equals("+")||command.equals("*")||command.equals("/")){
+                    display.setText("Error");
+                    start = true;
+                }
+                else if (command.equals("A/C")){
+                    display.setText("");
+                }
+                else if (command.equals("1")||command.equals("2")||command.equals("3")||
+                        command.equals("4")||command.equals("5")||command.equals("6")||
+                        command.equals("7")||command.equals("8")||command.equals("9")||
+                        command.equals("0")||command.equals("%")||command.equals("+/-")){
+                    start = false;
+                }
+                else lastCommand = command;
+            }
+
+            else{
+                calculate(Double.parseDouble(display.getText()));
+                lastCommand = command;
+                start = true;
+            }
+        }
+    }
+
+    /**
+     * Carries out the pending calculation.
+     * @param x the value to be accumulated with the prior result.
+     */
+    public void calculate(double x){
+        if(lastCommand.equals("+")) result +=x;
+        else if (lastCommand.equals("*")) result *=x;
+        else if (lastCommand.equals("-")) result -=x;
+        else if (lastCommand.equals("/")) result /=x;
+        else if (lastCommand.equals("=")) result =x;
+        else if (lastCommand.equals("A/C")) result=0;
+        display.setText(""+ result);
+
+    }
+}
+
